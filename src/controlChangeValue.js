0234b0607a5aa4a859851afca83d58a50e9af36d
Rodolfo Alvarez <atahualpa1970@outlook.com>
2022-06-13 18:40:43 -0300

    False

--- src/controlChangeValue.js
+++ src/controlChangeValue.js
@@ -1,6 +1,6 @@
 exports.changeValueC = (valueA, valueB) => {
     var valueC = ""
-    if (!valueA && !valueB) valueC = "FALSE"
+    if (!valueA && !valueB) valueC = false
     if (valueA && !valueB) valueC = "[ A ]"
     if (!valueA && valueB) valueC = "[ B ]"
     if (valueA && valueB) valueC = "TRUE"
